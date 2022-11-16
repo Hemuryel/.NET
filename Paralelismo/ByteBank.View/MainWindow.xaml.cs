d9e261eef4d1b8097cad2faa9c4c0d6f4b1ed378
Hemuryel <hyl.silva@gmail.com>
2022-11-16 17:52:31 -0300

    Alterações.

--- Paralelismo/ByteBank.View/MainWindow.xaml.cs
+++ Paralelismo/ByteBank.View/MainWindow.xaml.cs
@@ -34,88 +34,134 @@ namespace ByteBank.View
             r_Servico = new ContaClienteService();
         }
 
-        private void BtnProcessar_Click(object sender, RoutedEventArgs e)
+        //private async void BtnProcessar_Click(object sender, RoutedEventArgs e)
+        //{
+        //    var taskSchedulerUI = TaskScheduler.FromCurrentSynchronizationContext();
+        //    BtnProcessar.IsEnabled = false;
+
+        //    var contas = r_Repositorio.GetContaClientes();
+
+        //    var contasQuantidadePorThread = contas.Count() / 4;
+
+        //    var contasParte1 = contas.Take(contasQuantidadePorThread);
+        //    var contasParte2 = contas.Skip(contasQuantidadePorThread).Take(contasQuantidadePorThread);
+        //    var contasParte3 = contas.Skip(contasQuantidadePorThread * 2).Take(contasQuantidadePorThread);
+        //    var contasParte4 = contas.Skip(contasQuantidadePorThread * 3);
+
+        //    var resultado = new List<string>();
+
+        //    AtualizarView(new List<string>(), TimeSpan.Zero);
+
+        //    var inicio = DateTime.Now;
+
+        //    //foreach (var conta in contas)
+        //    //{
+        //    //    var resultadoConta = r_Servico.ConsolidarMovimentacao(conta);
+        //    //    resultado.Add(resultadoConta);
+        //    //}
+
+        //    //Thread threadParte1 = new Thread(() =>
+        //    //{
+        //    //    foreach (var conta in contasParte1)
+        //    //    {
+        //    //        var resultadoProcessamento = r_Servico.ConsolidarMovimentacao(conta);
+        //    //        resultado.Add(resultadoProcessamento);
+        //    //    }
+        //    //});
+
+        //    //Thread threadParte2 = new Thread(() =>
+        //    //{
+        //    //    foreach (var conta in contasParte2)
+        //    //    {
+        //    //        var resultadoProcessamento = r_Servico.ConsolidarMovimentacao(conta);
+        //    //        resultado.Add(resultadoProcessamento);
+        //    //    }
+        //    //});
+
+        //    //Thread threadParte3 = new Thread(() =>
+        //    //{
+        //    //    foreach (var conta in contasParte3)
+        //    //    {
+        //    //        var resultadoProcessamento = r_Servico.ConsolidarMovimentacao(conta);
+        //    //        resultado.Add(resultadoProcessamento);
+        //    //    }
+        //    //});
+
+        //    //Thread threadParte4 = new Thread(() =>
+        //    //{
+        //    //    foreach (var conta in contasParte4)
+        //    //    {
+        //    //        var resultadoProcessamento = r_Servico.ConsolidarMovimentacao(conta);
+        //    //        resultado.Add(resultadoProcessamento);
+        //    //    }
+        //    //});
+
+        //    //threadParte1.Start();
+        //    //threadParte2.Start();
+        //    //threadParte3.Start();
+        //    //threadParte4.Start();
+
+        //    //while (threadParte1.IsAlive || threadParte2.IsAlive || threadParte3.IsAlive || threadParte4.IsAlive)
+        //    //{
+        //    //    Thread.Sleep(250);
+        //    //}
+
+        //    var contasTarefas = contas.Select(conta =>
+        //    {
+        //        return Task.Factory.StartNew(() =>
+        //        {
+        //            //var resultadoConta = r_Servico.ConsolidarMovimentacao(conta);
+        //            //resultado.Add(resultadoConta);
+        //            return r_Servico.ConsolidarMovimentacao(conta);
+        //        });
+        //    }).ToArray();
+
+        //    //Task.WaitAll(contasTarefas);
+
+
+        //    //Task.WhenAll(contasTarefas)
+        //    //    .ContinueWith(task =>
+        //    //    {
+        //    //        var fim = DateTime.Now;
+        //    //        AtualizarView(resultado, fim - inicio);
+        //    //    }, taskSchedulerUI)
+        //    //    .ContinueWith(task =>
+        //    //    {
+        //    //        BtnProcessar.IsEnabled = true;
+        //    //    }, taskSchedulerUI);
+
+        //    // sem await
+        //    ConsolidarContas(contas).ContinueWith(task =>
+        //    {
+        //        var fim = DateTime.Now;
+        //        resultado = task.Result;
+        //        AtualizarView(resultado, fim - inicio);
+        //    }, taskSchedulerUI)
+        //        .ContinueWith(task =>
+        //        {
+        //            BtnProcessar.IsEnabled = true;
+        //        }, taskSchedulerUI);
+
+        //    var fim = DateTime.Now;
+        //    AtualizarView(resultado, fim - inicio);
+        //    BtnProcessar.IsEnabled = true;
+        //}
+
+        private async void BtnProcessar_Click(object sender, RoutedEventArgs e)
         {
+            BtnProcessar.IsEnabled = false;
+
             var contas = r_Repositorio.GetContaClientes();
-
-            var contasQuantidadePorThread = contas.Count() / 4;
-
-            var contasParte1 = contas.Take(contasQuantidadePorThread);
-            var contasParte2 = contas.Skip(contasQuantidadePorThread).Take(contasQuantidadePorThread);
-            var contasParte3 = contas.Skip(contasQuantidadePorThread*2).Take(contasQuantidadePorThread);
-            var contasParte4 = contas.Skip(contasQuantidadePorThread*3);
-
-            var resultado = new List<string>();
 
             AtualizarView(new List<string>(), TimeSpan.Zero);
 
             var inicio = DateTime.Now;
 
-            //foreach (var conta in contas)
-            //{
-            //    var resultadoConta = r_Servico.ConsolidarMovimentacao(conta);
-            //    resultado.Add(resultadoConta);
-            //}
-
-            //Thread threadParte1 = new Thread(() =>
-            //{
-            //    foreach (var conta in contasParte1)
-            //    {
-            //        var resultadoProcessamento = r_Servico.ConsolidarMovimentacao(conta);
-            //        resultado.Add(resultadoProcessamento);
-            //    }
-            //});
-
-            //Thread threadParte2 = new Thread(() =>
-            //{
-            //    foreach (var conta in contasParte2)
-            //    {
-            //        var resultadoProcessamento = r_Servico.ConsolidarMovimentacao(conta);
-            //        resultado.Add(resultadoProcessamento);
-            //    }
-            //});
-
-            //Thread threadParte3 = new Thread(() =>
-            //{
-            //    foreach (var conta in contasParte3)
-            //    {
-            //        var resultadoProcessamento = r_Servico.ConsolidarMovimentacao(conta);
-            //        resultado.Add(resultadoProcessamento);
-            //    }
-            //});
-
-            //Thread threadParte4 = new Thread(() =>
-            //{
-            //    foreach (var conta in contasParte4)
-            //    {
-            //        var resultadoProcessamento = r_Servico.ConsolidarMovimentacao(conta);
-            //        resultado.Add(resultadoProcessamento);
-            //    }
-            //});
-
-            //threadParte1.Start();
-            //threadParte2.Start();
-            //threadParte3.Start();
-            //threadParte4.Start();
-
-            //while (threadParte1.IsAlive || threadParte2.IsAlive || threadParte3.IsAlive || threadParte4.IsAlive)
-            //{
-            //    Thread.Sleep(250);
-            //}
-
-            var contasTarefas = contas.Select(conta => 
-            {
-                return Task.Factory.StartNew(() => 
-                {
-                    var resultadoConta = r_Servico.ConsolidarMovimentacao(conta);
-                    resultado.Add(resultadoConta);
-                });
-            }).ToArray();
-
-            Task.WaitAll(contasTarefas);
+            var resultado = await ConsolidarContas(contas);
 
             var fim = DateTime.Now;
             AtualizarView(resultado, fim - inicio);
+            BtnProcessar.IsEnabled = true;
         }
 
         //private async void BtnProcessar_Click(object sender, RoutedEventArgs e)
@@ -160,24 +206,53 @@ namespace ByteBank.View
             _cts.Cancel();
         }
 
-        private async Task<string[]> ConsolidarContas(IEnumerable<ContaCliente> contas, IProgress<string> reportadorDeProgresso, CancellationToken ct)
+        //private Task<List<string>> ConsolidarContas(IEnumerable<ContaCliente> contas)
+        //{
+        //    var resultado = new List<string>();
+        //    var tasks = contas.Select(conta =>
+        //    {
+        //        return Task.Factory.StartNew(() =>
+        //        {
+        //            var contaResultado = r_Servico.ConsolidarMovimentacao(conta);
+        //            resultado.Add(contaResultado);
+        //        });
+        //    });
+
+        //    return Task.WhenAll(tasks).ContinueWith(t =>
+        //    {
+        //        return resultado;
+        //    });
+        //}
+
+        private async Task<string[]> ConsolidarContas(IEnumerable<ContaCliente> contas)
         {
             var tasks = contas.Select(conta =>
-                Task.Factory.StartNew(() =>
-                {
-                    ct.ThrowIfCancellationRequested();
-
-                    var resultadoConsolidacao = r_Servico.ConsolidarMovimentacao(conta, ct);
-
-                    reportadorDeProgresso.Report(resultadoConsolidacao);
-
-                    ct.ThrowIfCancellationRequested();
-                    return resultadoConsolidacao;
-                }, ct)
+                Task.Factory.StartNew(() => r_Servico.ConsolidarMovimentacao(conta))
             );
 
-            return await Task.WhenAll(tasks);
+            var resultado = await Task.WhenAll(tasks);
+
+            return resultado;
         }
+
+        //private async Task<string[]> ConsolidarContas(IEnumerable<ContaCliente> contas, IProgress<string> reportadorDeProgresso, CancellationToken ct)
+        //{
+        //    var tasks = contas.Select(conta =>
+        //        Task.Factory.StartNew(() =>
+        //        {
+        //            ct.ThrowIfCancellationRequested();
+
+        //            var resultadoConsolidacao = r_Servico.ConsolidarMovimentacao(conta, ct);
+
+        //            reportadorDeProgresso.Report(resultadoConsolidacao);
+
+        //            ct.ThrowIfCancellationRequested();
+        //            return resultadoConsolidacao;
+        //        }, ct)
+        //    );
+
+        //    return await Task.WhenAll(tasks);
+        //}
 
         private void LimparView()
         {
@@ -188,7 +263,7 @@ namespace ByteBank.View
 
         private void AtualizarView(IEnumerable<String> result, TimeSpan elapsedTime)
         {
-            var tempoDecorrido = $"{ elapsedTime.Seconds }.{ elapsedTime.Milliseconds} segundos!";
+            var tempoDecorrido = $"{elapsedTime.Seconds}.{elapsedTime.Milliseconds} segundos!";
             var mensagem = $"Processamento de {result.Count()} clientes em {tempoDecorrido}";
 
             LstResultados.ItemsSource = result;

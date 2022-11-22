84882be7915243ce6fb31458b898d095f5543351
Hemuryel <hyl.silva@gmail.com>
2022-11-22 15:35:24 -0300

    Alterações.

---
+++ LINQ/LinqToXml/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Xml.Linq;
+
+namespace AluraTunesLinqToXml
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            XElement root = XElement.Load(@"XMLFile.xml");
+
+            var queryXML =
+                from g in root.Element("Generos").Elements("Genero")
+                select g;
+
+            foreach (var genero in queryXML)
+            {
+                Console.WriteLine($"{genero.Element("GeneroId").Value}\t{genero.Element("Nome").Value}");
+            }
+        }
+    }
+}

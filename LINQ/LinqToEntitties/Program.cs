9dacd3f105be3325491805db49ec8ac719ddfcc4
Hemuryel <hyl.silva@gmail.com>
2022-11-23 14:47:59 -0300

    Alterações.

---
+++ LINQ/LinqToEntitties/Program.cs
@@ -0,0 +1,20 @@
+﻿using LinqToEntities.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqToEntities
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var contexto = new AluraTunesEntities())
+            {
+
+            }
+        }
+    }
+}

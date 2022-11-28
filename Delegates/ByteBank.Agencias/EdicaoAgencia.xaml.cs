b0b31b6abb23eb2bb49c90a014cfb1e7b7429c08
Hemuryel <hyl.silva@gmail.com>
2022-11-28 09:05:07 -0300

    Alterações.

---
+++ Delegates/ByteBank.Agencias/EdicaoAgencia.xaml.cs
@@ -0,0 +1,93 @@
+﻿using ByteBank.Agencias.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ByteBank.Agencias
+{
+    /// <summary>
+    /// Interaction logic for EdicaoAgencia.xaml
+    /// </summary>
+    public partial class EdicaoAgencia : Window
+    {
+        private readonly Agencia _agencia;
+
+        public EdicaoAgencia(Agencia agencia)
+        {
+            InitializeComponent();
+
+            _agencia = agencia ?? throw new ArgumentNullException(nameof(agencia));
+            AtualizarCamposDeTexto();
+            AtualizarControles();
+
+            ValidarCampoNulo(txtNumero, EventArgs.Empty);
+            ValidarSomenteDigito(txtNumero, EventArgs.Empty);
+
+            ValidarCampoNulo(txtNome, EventArgs.Empty);
+            ValidarCampoNulo(txtTelefone, EventArgs.Empty);
+            ValidarCampoNulo(txtEndereco, EventArgs.Empty);
+            ValidarCampoNulo(txtDescricao, EventArgs.Empty);
+        }
+
+        private void AtualizarCamposDeTexto()
+        {
+            txtNumero.Text = _agencia.Numero;
+            txtNome.Text = _agencia.Nome;
+            txtTelefone.Text = _agencia.Telefone;
+            txtEndereco.Text = _agencia.Endereco;
+            txtDescricao.Text = _agencia.Descricao;
+        }
+
+        private void AtualizarControles()
+        {
+            RoutedEventHandler dialogResultTrue = (o, e) => DialogResult = true;
+            RoutedEventHandler dialogResultFalse = (o, e) => DialogResult = false;
+
+            var okEventHandler = dialogResultTrue + Fechar;
+            var cancelarEventHandler = dialogResultFalse + Fechar;
+
+            btnOk.Click += okEventHandler;
+            btnCancelar.Click += cancelarEventHandler;
+
+            txtNome.TextChanged += ValidarCampoNulo;
+            txtDescricao.TextChanged += ValidarCampoNulo;
+            txtEndereco.TextChanged += ValidarCampoNulo;
+            txtNumero.TextChanged += ValidarSomenteDigito;
+            txtTelefone.TextChanged += ValidarCampoNulo;
+        }
+
+        private void ValidarSomenteDigito(object sender, EventArgs e)
+        {
+            var txt = sender as TextBox;
+
+            var caracteresSaoDigitos = txt.Text.All(Char.IsDigit);
+
+            txt.Background = caracteresSaoDigitos
+                ? new SolidColorBrush(Colors.White)
+                : new SolidColorBrush(Colors.OrangeRed);
+        }
+
+        private void ValidarCampoNulo(object sender, EventArgs e)
+        {
+            var txt = sender as TextBox;
+            var textoEstaVazio = String.IsNullOrEmpty(txt.Text);
+
+            txt.Background = textoEstaVazio
+                ? new SolidColorBrush(Colors.OrangeRed)
+                : new SolidColorBrush(Colors.White);
+        }
+
+        private void Fechar(object sender, EventArgs e) =>
+            Close();
+    }
+}
--- Delegates/ByteBank.Agencias/MainWindow.xaml.cs
+++ Delegates/ByteBank.Agencias/MainWindow.xaml.cs
@@ -29,7 +29,9 @@ namespace ByteBank.Agencias
             InitializeComponent();
 
             lstAgencias = new ListBox();
+
             AtualizarControles();
+            AtualizarListaDeAgencias();
         }
 
         private void AtualizarControles()
@@ -37,21 +39,56 @@ namespace ByteBank.Agencias
             lstAgencias.Width = 270;
             lstAgencias.Height = 290;
 
-            //margin
             Canvas.SetTop(lstAgencias, 15);
             Canvas.SetLeft(lstAgencias, 15);
 
+            lstAgencias.SelectionChanged += new SelectionChangedEventHandler(lstAgencias_SelectionChanged);
+
             container.Children.Add(lstAgencias);
 
+            btnEditar.Click += new RoutedEventHandler(btnEditar_Click);
+        }
+
+        private void btnEditar_Click(object sender, RoutedEventArgs e)
+        {
+            var agenciaAtual = (Agencia)lstAgencias.SelectedItem;
+            var janelaEdicao = new EdicaoAgencia(agenciaAtual);
+
+            var resultado = janelaEdicao.ShowDialog().Value;
+
+            if (resultado)
+            {
+                // Usuario clicou em Ok
+            }
+            else
+            {
+                // Usuario clicou em Cancelar
+            }
+
+        }
+
+        private void AtualizarListaDeAgencias()
+        {
             lstAgencias.Items.Clear();
             var agencias = _contextoBancoDeDados.Agencias.ToList();
             foreach (var agencia in agencias)
                 lstAgencias.Items.Add(agencia);
         }
 
+        private void lstAgencias_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var agenciaSelecionada = (Agencia)lstAgencias.SelectedItem;
+
+            txtNumero.Text = agenciaSelecionada.Numero;
+            txtNome.Text = agenciaSelecionada.Nome;
+            txtTelefone.Text = agenciaSelecionada.Telefone;
+            txtEndereco.Text = agenciaSelecionada.Endereco;
+            txtDescricao.Text = agenciaSelecionada.Descricao;
+        }
+
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            var confirmacao = 
+            var confirmacao =
                 MessageBox.Show(
                     "Você deseja realmente excluir este item?",
                     "Confirmação",
